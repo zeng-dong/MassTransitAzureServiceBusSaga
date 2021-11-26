aed09aa379274dcb0af2404c6610c0386ea5b4b4
Dong Zeng <dong@freshbyte.com>
2021-11-26 09:13:34 -0600

    OrderController

---
+++ src/Api/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController :
+        ControllerBase
+{
+    readonly IRequestClient<SubmitOrder> _client;
+    readonly ILogger<OrderController> _logger;
+
+    public OrderController(ILogger<OrderController> logger, IRequestClient<SubmitOrder> client)
+    {
+        _logger = logger;
+        _client = client;
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Put(OrderModel order)
+    {
+        Response<OrderSubmissionAccepted> response = await _client.GetResponse<OrderSubmissionAccepted>(new SubmitOrder
+        {
+            OrderId = order.OrderId,
+            OrderNumber = order.OrderNumber,
+            Timestamp = DateTimeOffset.Now
+        });
+
+        return Accepted(response.Message);
+    }
+}
+
+
+public record OrderModel
+{
+    public Guid OrderId { get; init; }
+    public string OrderNumber { get; init; }
+}
